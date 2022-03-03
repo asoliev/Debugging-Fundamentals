2de64e3ed707569468fb742e7e3fbcb703325493
Abdukodir <Abdukodir_Soliev@epam.com>
2022-03-03 23:47:47 +0500

    fix IndexOf method

--- Task1/Utilities.cs
+++ Task1/Utilities.cs
@@ -39,12 +39,21 @@ namespace Task1
         /// otherwise -1.</returns>
         public static int IndexOf(Product[] products, Predicate<Product> predicate)
         {
-            for (int i = 0; i < products.Length - 1; i++)
+            if (products == null)
             {
-                var product = products[i - 1];
+                throw new ArgumentNullException(nameof(products));
+            }
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                var product = products[i];
                 if (predicate(product))
                 {
-                    return --i;
+                    return i;
                 }
             }
 
