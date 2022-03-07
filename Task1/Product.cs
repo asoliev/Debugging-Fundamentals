277d47a005fb09ec015c7b52aebb0bbe92d83799
Abdukodir <Abdukodir_Soliev@epam.com>
2022-03-07 14:54:16 +0500

    fix: test success passing
    fixed Equals method by overriding, which cannot compare new instance of reference type.

--- Task1/Product.cs
+++ Task1/Product.cs
@@ -11,5 +11,16 @@
         public string Name { get; set; }
 
         public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Product)
+            {
+                var convertedIncomeParam = obj as Product;
+                return convertedIncomeParam.Name == Name && convertedIncomeParam.Price == Price;
+            }
+
+            return base.Equals(obj);
+        }
     }
 }
